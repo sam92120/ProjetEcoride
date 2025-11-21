f03569990e0c27f8bc60b48c90ddb22006b338cd
metelus <samuelos92120@gmail.com>
2025-11-21 20:36:39 +0100

    correction

--- public/index.php
+++ public/index.php
@@ -1,9 +1,26 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    // Active le mode debug si nécessaire
+    if ($context['APP_DEBUG'] ?? false) {
+        umask(0000);
+        Debug::enable();
+    }
+
+    // Initialise le noyau de Symfony
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+
+    // Gère la requête HTTP
+    $request = Request::createFromGlobals();
+    $response = $kernel->handle($request);
+    $response->send();
+
+    // Termine la requête
+    $kernel->terminate($request, $response);
 };
